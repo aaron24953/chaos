706f6faa2633786a64b458f82043d9221c8118e0
aaron24953 <85455088+aaron24953@users.noreply.github.com>
2022-02-20 13:29:38 +0000

    reduced overall heat

--- particle sim.py
+++ particle sim.py
@@ -27,11 +27,11 @@ class particle:
     self.pos[0]+=self.vel[0]
     self.pos[1]+=self.vel[1]
     if self.state==0:
-      self.vel[0]*=(1-0.08/UPS)
-      self.vel[1]*=(1-0.08/UPS)
+      self.vel[0]*=(1-0.1/UPS)
+      self.vel[1]*=(1-0.1/UPS)
     if self.state==1:
-      self.vel[0]*=(1+0.096/UPS)
-      self.vel[1]*=(1+0.096/UPS)
+      self.vel[0]*=(1+0.11/UPS)
+      self.vel[1]*=(1+0.11/UPS)
 
   def launch(self,ang,vel,pos):
     self.pos[0]=pos[0]
