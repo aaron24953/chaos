524a63504f9752963999f9d56216603de5be6b9c
aaron24953 <85455088+aaron24953@users.noreply.github.com>
2022-03-10 17:19:09 +0000

    actually works now

--- instantNerdle.py
+++ instantNerdle.py
@@ -1,80 +1,51 @@
 from permutations import permute
 
+def calc(toCheck,operand,i):
+    temp=toCheck
+    a=int(toCheck[i-1])
+    try:
+        if i-2>=0:
+            a+=10*int(toCheck[i-2])
+    except:
+        pass
+    b=int(toCheck[i+1])
+    try:
+        b=10*int(toCheck[i+1])
+        b+=int(toCheck[i+2])
+    except:
+        b=int(toCheck[i+1])
+    #print(a,toCheck,b)
+    if operand=="*":
+        value=a*b
+    elif operand=="/":
+        value=a//b
+    elif operand=="+":
+        value=a+b
+    else:
+        value=a-b
+    toCheck="".join([temp[j] for j in range(0,i-len(str(a)))])
+    toCheck+=str(value)
+    toCheck+="".join([temp[j] for j in range(i+len(str(b))+1,len(temp))])
+    return toCheck
+
 def checkCalc(toCheck):
     bothSides=toCheck.split("=")
     toCheck=bothSides[0]
     for i in range(len(toCheck)):
         if toCheck[i] == "*":
-            temp=toCheck
-            a=int(toCheck[i-1])
-            try:
-                if i-2>=0:
-                    a+=10*int(toCheck[i-2])
-            except:
-                pass
-            b=int(toCheck[i+1])
-            try:
-                b+=10*int(toCheck[i+2])
-            except:
-                pass
-            value=a*b
-            toCheck="".join([temp[j] for j in range(0,i-len(str(a)))])
-            toCheck+=str(value)
-            toCheck+="".join([temp[j] for j in range(i+len(str(b))+1,len(temp))])
+            toCheck=calc(toCheck,toCheck[i],i)
             break
     for i in range(len(toCheck)):
         if toCheck[i] == "/":
-            temp=toCheck
-            a=int(toCheck[i-1])
-            try:
-                a+=10*int(toCheck[i-2])
-            except:
-                pass
-            b=int(toCheck[i+1])
-            try:
-                b+=10*int(toCheck[i+2])
-            except:
-                pass
-            value=a//b
-            toCheck="".join([temp[j] for j in range(0,i-len(str(a)))])
-            toCheck+=str(value)
-            toCheck+="".join([temp[j] for j in range(i+len(str(b))+1,len(temp))])
+            toCheck=calc(toCheck,toCheck[i],i)
             break
     for i in range(len(toCheck)):
         if toCheck[i] == "+":
-            temp=toCheck
-            a=int(toCheck[i-1])
-            try:
-                a+=10*int(toCheck[i-2])
-            except:
-                pass
-            b=int(toCheck[i+1])
-            try:
-                b+=10*int(toCheck[i+2])
-            except:
-                pass
-            value=a+b
-            toCheck="".join([temp[j] for j in range(0,i-len(str(a)))])
-            toCheck+=str(value)
-            toCheck+="".join([temp[j] for j in range(i+len(str(b))+1,len(temp))])
+            toCheck=calc(toCheck,toCheck[i],i)
             break
     for i in range(len(toCheck)):
         if toCheck[i] == "-":
-            temp=toCheck
-            a=int(toCheck[i-1])
-            try:
-                a+=10*int(toCheck[i-2])
-            except:
-                pass
-            b=int(toCheck[i+1])
-            try:
-                b+=10*int(toCheck[i+2])
-            except:
-                pass
-            value=a-b
-            toCheck="".join([temp[j] for j in range(0,i-len(str(a)))])
-            toCheck+=str(value)
-            toCheck+="".join([temp[j] for j in range(i+len(str(b))+1,len(temp))])
+            toCheck=calc(toCheck,toCheck[i],i)
             break
     if toCheck==bothSides[1]:
         return True
@@ -87,7 +58,7 @@ def checkValidNerdle(toCheck):
             eq+=1
         else:
             break
-    if eq<5 or eq==7:
+    if eq<len(toCheck)-3 or eq==len(toCheck)-1:
         return False
     symbol=0
     symbols=[]
@@ -108,7 +79,6 @@ def main():
     stuff="75*=34-8"
     stuff=[i for i in stuff]
     perms=permute(stuff)
-    print("perms made")
     for i in perms:
         if checkValidNerdle(i):
             print("".join(i))
