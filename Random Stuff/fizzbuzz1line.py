d3f2b6b54bbab0947d86fd5c5dbc85c57d0ea103
aaron24953 <85455088+aaron24953@users.noreply.github.com>
2022-11-02 08:52:57 +0000

    idk

--- Random Stuff/fizzbuzz1line.py
+++ Random Stuff/fizzbuzz1line.py
@@ -1,7 +1,9 @@
 import time
 
+numI = 1
+numP = 100
 b: list[float] = []
-for j in range(100):
+for j in range(numI):
     start_time = time.time()
     [
         "fizzbuzz"
@@ -11,18 +13,19 @@ for j in range(100):
         else "buzz"
         if i % 5 == 0
         else i
-        for i in range(1, 10001)
+        for i in range(1, numP+1)
     ]
     b.append(time.time() - start_time)
 bl: list[float] = []
-for j in range(100):
+for j in range(numI):
     start_time = time.time()
-    [
-        "fizz" * int(i // 3 == (i + 2) // 3)
-        + "buzz" * int(i // 5 == (i + 4) // 5)
-        + str(i) * (1 - int(i // 5 == (i + 4) // 5) - int(i // 3 == (i + 2) // 3))
-        for i in range(10001)
+    p=[
+        "fizz" * (1+i // 3 - (i + 2) // 3)
+        + "buzz" * (1+i // 5 - (i + 4) // 5)
+        + str(i) * (1 - (1 + i // 5 - (i + 4) // 5) - (1+i // 3 - (i + 2) // 3))
+        for i in range(1,numP+1)
     ]
+    print(p)
     bl.append(time.time() - start_time)
 
 print(f"{sum(b)},{sum(b)/len(b)}\n{sum(bl)},{sum(bl)/len(bl)}")
