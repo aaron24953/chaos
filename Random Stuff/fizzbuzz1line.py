b9fd3fbfe122998f33bd384691a45d25beffa818
aaron24953 <85455088+aaron24953@users.noreply.github.com>
2022-11-03 09:48:02 +0000

    sdf

--- Random Stuff/fizzbuzz1line.py
+++ Random Stuff/fizzbuzz1line.py
@@ -23,7 +23,7 @@ for j in range(numI):
         "fizz" * (1+i // 3 - (i + 2) // 3)
         + "buzz" * (1+i // 5 - (i + 4) // 5)
         + str(i) * (1 - (1 + i // 5 - (i + 4) // 5) - (1+i // 3 - (i + 2) // 3))
-        for i in range(1,numP+1)
+        for i in range(1, numP+1)
     ]
     print(p)
     bl.append(time.time() - start_time)
