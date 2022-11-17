8c194235f9d174553e8a769fc16d964a0e6dca41
aaron24953 <85455088+aaron24953@users.noreply.github.com>
2022-11-17 12:44:49 +0000

    dev

--- Coursework/login.py
+++ Coursework/login.py
@@ -11,15 +11,23 @@ class ExamplePage(object):
         Dialog.exec()
 
     def setupUI(self, Dialog):
-        self.x=config.X/2
-        self.y=config.Y
+        self.x=config.X/3
+        self.y=config.Y/2
         Dialog.resize(self.x, self.y)
-        Dialog.setWindowTitle("login")
+        Dialog.setWindowTitle("Login")
+
         self.pushButton=QtWidgets.QPushButton(Dialog)
-        self.pushButton.setFixedSize(self.x/3, self.y/3)
-        self.pushButton.setText("example PB")
-        self.pushButton.move(self.x/3, self.y/3)
-        self.pushButton.clicked.connect(self.randomisePB)
+        self.pushButton.setFixedSize(self.x/3, self.y/6)
+        self.pushButton.setText("Login")
+        self.pushButton.move(self.x/3, self.y*5/6)
+        #  self.pushButton.clicked.connect(self.randomisePB)
+
+        self.loginText = QtWidgets.QLabel(Dialog)
+        self.loginText.setText("Please Log In")
+        self.loginText.setAlignment(QtCore.Qt.AlignCenter)
+        self.loginText.resize(self.x/5, self.y/5)
+        self.loginText.move(self.x*2/5, 0)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
